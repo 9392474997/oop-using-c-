875066a15d18ffe123ca13178b307fc3493a569e
9392474997 <112842874+9392474997@users.noreply.github.com>
2022-09-05 13:33:31 +0530

    Add files via upload

---
+++ vishnu5588.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+int main()
+{
+	int a,b;
+	a=10;
+	b=5;
+	
+	if(a>b)
+	{
+		std::cout<<"A is grater";
+	}
+	else if(b>a)
+	{
+		std::cout<<"b is greater";
+		
+	}
+	else
+	{
+		std::cout<<"invalid input";
+	}
+		return 0;
+}
+
